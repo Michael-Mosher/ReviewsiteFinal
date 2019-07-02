e303d194062a2c77a001a04be3d8443b75b9b232
Michael-Mosher <michael.r.mosher@gmail.com>
2019-07-02 17:01:43 -0400

    Resolved Review link and Nav-bar spacing

--- src/main/java/com/wecancodeit/reviewssite/Review.java
+++ src/main/java/com/wecancodeit/reviewssite/Review.java
@@ -43,71 +43,57 @@ class Review {
     return this.id;
   }
 
-  
+  /**
+   * @return the author
+   */
+  public String getAuthor()
+  {
+	return this.author;
+  }
 
-//	public int compareTo(Review oFirstCourse) {
-//		if(this.getName().compareTo(oFirstCourse.getName()) !=0) {
-//			return this.getName().compareTo(oFirstCourse.getName());
-//		} else { // Name the same
-//			if(this.getDescription().compareTo(oFirstCourse.getDescription())!=0) {
-//				return this.getDescription().compareTo(oFirstCourse.getDescription());
-//			} else {
-//				return 0;
-//			}
-//		}
-//	}
+  /**
+   * @return the content
+   */
+  public String getContent()
+  {
+    return this.content;
+  }
 
-	/**
- * @return the author
- */
-public String getAuthor() {
-	return author;
-}
+  /**
+   * @return the reviewTitle
+   */
+  public String getReviewTitle()
+  {
+    return this.reviewTitle;
+  }
 
-/**
- * @return the content
- */
-public String getContent() {
-	return content;
-}
+  /**
+   * @return the game
+   */
+  public Game getGame()
+  {
+    return game;
+  }
 
-/**
- * @return the reviewTitle
- */
-public String getReviewTitle() {
-	return reviewTitle;
-}
-/**
- * @return the game
- */
-public Game getGame() {
-	return game;
-}
+  @Override
+  public int hashCode()
+  {
+    final int prime = 31;
+    int result = 1;
+    result = (int) (prime * result + ((this.getId() == 0) ? 0 : this.getId()));
+    return result;
+  }
 
-	@Override
-	public int hashCode()
-	{
-		final int prime = 31;
-		int result = 1;
-		result = (int) (prime * result + ((id == 0) ? 0 : id));
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj)
-	{
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (this.getClass() != obj.getClass())
-			return false;
+  @Override
+  public boolean equals(Object obj)
+  {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (this.getClass() != obj.getClass()) return false;
 		Review other = (Review) obj;
-		if (this.id == 0) {
-		  if (other.id != 0)
-			return false;
-		} else if (id!=other.id)
-			return false;
-		return true;
-	}
+    if (this.id == 0) {
+      if (other.id != 0) return false;
+    } else if (this.getId()!=other.id) return false;
+    return true;
+  }
 }
